7f7996609ba4da429f6eccb140c4bfc243e5c521
Elizaveta <Elizavetabelyankina@mail.ru>
2023-07-27 17:29:02 +0300

    g

---
+++ liza1.java
@@ -0,0 +1,7 @@
+public class liza1 {
+    public static void main(String[] args) {
+        System.out.println("Hello\nworld\"lflf\""); // \n - переход на новую строку \t - отступ слева типа табуляция
+        System.out.println("f"); // \" - так выводим кавычку
+        System.out.println("f");
+    }
+}
---
+++ liza3.java
@@ -0,0 +1,3 @@
+public class liza3 {
+    
+}
