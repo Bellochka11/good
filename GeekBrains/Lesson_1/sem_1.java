e7c4093582056875eafb8f199962c5184c05000b
Elizaveta <Elizavetabelyankina@mail.ru>
2023-09-13 00:36:32 +0300

    g

--- GeekBrains/Lesson_1/sem_1.java
+++ GeekBrains/Lesson_1/sem_1.java
@@ -1,5 +1,24 @@
 package GeekBrains.Lesson_1;
 
+// дан массив чисел [1,1,0,1,1,1] вывести максимальное кол-во подряд идущих 1
 public class sem_1 {
-    
+    public static void main(String[] args) {
+        int[] arr = new int[] { 1, 1, 0, 1, 1, 1 };
+        int count = 0;
+        int current = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) {
+                current++;
+            } else {
+                if (current > count) {
+                    count = current;
+                }
+                current = 0;
+            }
+        }
+        if (current > count) { //проверяем т.к. не встретили 0 в конце
+            count = current;
+        }
+        System.out.println(count);
+    }
 }
--- GeekBrains/Lesson_1/sem_2.java
+++ GeekBrains/Lesson_1/sem_2.java
@@ -1,5 +1,46 @@
 package GeekBrains.Lesson_1;
 
+import java.util.Arrays;
+
+//Дан массив array = [3,2,4,3,3,1,5,3,3] и число val = 3.
+// Если в массиве есть числа, равные заданному, нужно перенести эти элементы в конец массива.
+// Таким образом, первые несколько (или все) элементов массива должны быть отличны от заданного, а остальные - равны ему.
+// [2 4 1 5 3 3 3 3 3]
+// Работать разрешается только с 1 массивом.
 public class sem_2 {
-    
+    public static void main(String[] args) {
+        int[] array = { 3, 2, 4, 3, 3, 1, 5, 3, 3 };
+        int value = 3;
+        int swp = 0;
+        // int counter = 0;
+        // for (int i = 0; i < array.length; i++) {
+        //     if (array[i] == 3) {
+        //         counter += 1;
+        //     }
+        // }
+        // for (int i = 0; i < array.length - counter; i++) {
+        //     if (array[i] == value) {
+        //         for (int j = i; j < array.length - 1; j++) {// смещение элементов влево
+        //             array[j] = array[j + 1]; // перемещение влево
+        //         }
+        //         array[array.length - 1] = value; // последний элемент = 3
+        //         i--;
+        //     }
+        // }
+        for (int i = 1; i < array.length; i++) { //пузырек
+            for (int j = i; j < array.length; j++) {
+                if (array[j - 1] == 3) {
+                    swp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = swp;
+
+                }
+            }
+
+            System.out.println(Arrays.toString(array));
+
+        }
+        // System.out.println(Arrays.toString(array));
+        
+    }
 }
